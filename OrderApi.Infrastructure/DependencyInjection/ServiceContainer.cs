14a116bdb978f1d92d457f5958a25981fb8dc7ac
Dogu_ <dogu_rog@hotmail.com>
2025-05-14 09:38:09 +0300

    add dependency injection

---
+++ OrderApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,43 @@
+﻿using eCommerceSharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderApi.Application.Interfaces;
+using OrderApi.Infrastructure.Data;
+using OrderApi.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+
+            //add database connectivity
+            //add authentication scheme
+            SharedServiceContainer.AddSharedServices<OrderDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            //Create Dependency Injection
+            services.AddScoped<IOrder, OrderRepository>();
+
+            return services;
+
+        }
+
+        public static IApplicationBuilder UserInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //register middleware such as:
+            //global exception -> handle external errors
+            //ListenToApiGateway Only -> block all outsiders calls
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+        }
+
+    }
+}
